d5faf204103471d874bae466d252cdc167abea3f
AngelaFig <angelafigueroa596@gmail.com>
2023-10-27 19:25:52 -0400

    added info to auth.js, server.js and added apollo server to package json

--- server/schemas/typedefs.js
+++ server/schemas/typedefs.js
@@ -1,6 +1,6 @@
-const {Gql} = require('apollo-server-express');
+const {gql} = require('apollo-server-express');
 
-const typeDefs = Gql`
+const typeDefs = gql`
 type User{
     _id:ID
     username: String
